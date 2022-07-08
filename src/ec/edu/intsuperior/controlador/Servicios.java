14ed356418f3519daf6aee8cdfab522e1f0e74f9
User <User@DESKTOP-0A5SAM6>
2022-07-07 19:23:07 -0500

    Esta es el proyecto de la base de la POO

---
+++ src/ec/edu/intsuperior/controlador/Servicios.java
@@ -0,0 +1,14 @@
+
+package ec.edu.intsuperior.controlador;
+
+/**
+ *
+ * @author User
+ */
+public class Servicios {
+    public int pow(int a1, int a2){
+    return (int) Math.pow (a2,a2);
+   
+}
+}
+
